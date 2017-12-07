c1e87d0d3dfa241952eed416a87f8c60bbf7f728
Judy McGoldrick <jjeanius@verizon.net>
2017-12-07 03:13:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,9 @@ def line(katz_deli)
     else
       line = "The line is currently:"
       katz_deli.each.with_index(1) do |person, idx|
-        
+
         line << " #{idx}. #{person}"
-    binding.pry
+    
   end
     puts line
     end
