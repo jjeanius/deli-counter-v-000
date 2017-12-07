538c06c14132828c079cde113b1cdfaeebe2ac58
Judy McGoldrick <jjeanius@verizon.net>
2017-12-07 01:46:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
       puts "The line is currently empty."
     elsif katz_deli.length == other_deli.length
       puts "The line is currently:#{other_deli.length}. #{other_deli}".
-
+end
 end
 end
 
