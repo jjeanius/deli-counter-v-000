19fa0382cf389c016cfb32be10ff558dd4c2c3cf
Judy McGoldrick <jjeanius@verizon.net>
2017-12-07 01:22:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,12 @@ deli_counter =[
   ]
 
 def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  elsif katz_deli == another_deli
-    puts "The line is currently: #{katz_deli.length} #{another_deli.join}. #{other_deli.join}."
-    end
+  if katz_deli == another_deli.join
+    puts "The line is currently: #{katz_deli.length} #{katz_deli}."
+    
+    if katz_deli.length == 0
+      puts "The line is currently empty."
+end
 end
 
 def take_a_number(katz_deli, name)
