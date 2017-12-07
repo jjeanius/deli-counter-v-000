6ea1557599c1f3dd88794e8ccf5faea53d36f88a
Judy McGoldrick <jjeanius@verizon.net>
2017-12-07 03:08:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,9 +10,13 @@ def line(katz_deli)
     if katz_deli.length == 0
       puts "The line is currently empty."
     else
+      line = "The line is currently:"
       katz_deli.each.with_index(1) do |person, idx|
-        binding.pry
-      end
+        
+        line << " #{idx}. #{person}"
+    binding.pry
+  end
+    puts line
     end
 end
 
