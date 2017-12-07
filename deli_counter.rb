491f37f2ab8df5c7ff4680c1b734679c4fd9d4d4
Judy McGoldrick <jjeanius@verizon.net>
2017-12-07 02:45:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,6 +24,5 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
     elsif katz_deli = other_deli.shift
-      puts "Currently serving Logan."
-  end
+      
 end
